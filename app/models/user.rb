487b395f1ab29e0d7d001064de005ce59bbf5432
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-10-18 19:11:11 +0200

    bugfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
-  def self.create_with_omniauth
+  def self.create_with_omniauth auth
     create! do |user|
       user.provider = auth['provider']
       user.uid = auth['uid']
