7cd34efc9f12e3d3c7dfd90d14c268fff08509b7
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-10-18 19:15:07 +0200

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ActiveRecord::Base
-  enum role: [:user, :vip, :admin]
+  enum role: [:user, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
   def self.create_with_omniauth auth
     create! do |user|
       user.provider = auth['provider']
       user.uid = auth['uid']
+      user.email = auth['email']
     end
+  end
+
+  def set_default_role
+    self.role = :user
   end
 
 end
