5357ea49b9fd86c4a6671a4d24a4be9459f13b98
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-10-18 12:23:51 +0200

    production es

--- config/initializers/chewy.rb
+++ config/initializers/chewy.rb
@@ -1,3 +1,3 @@
 Chewy.configuration = {host: 'localhost:9200'} if Rails.env.development?
 Chewy.configuration = {host: 'localhost:9200', prefix: 'test'} if Rails.env.test?
-Chewy.configuration = ENV['BONSAI_URL'] if Rails.env.production?
+Chewy.configuration = {host: ENV['BONSAI_URL']} if Rails.env.production?
