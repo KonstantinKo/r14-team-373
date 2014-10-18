d882de245094a5dda04fc9493bf6f7cf04567bb6
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-10-18 21:21:06 +0200

    heroku name

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,9 @@ class User < ActiveRecord::Base
     puts auth
     create! do |user|
       user.provider = auth['provider']
-
       user.uid = auth['uid']
       user.email = auth['info']['email']
-      user.nickname = auth['info']['nickname']
-
+      user.nickname = auth['info']['nickname'] || auth['info']['name']
     end
   end
 
