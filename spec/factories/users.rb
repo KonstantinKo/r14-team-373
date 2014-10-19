52cc7a6ec4cca85b510fed55472e02c809e19ddf
Martin Meyerhoff <mamhoff@googlemail.com>
2014-10-19 13:29:23 +0200

    a factory for users with treasures

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,16 @@
 FactoryGirl.define do
   factory :user do
-    name "Test User"
+    nickname "Test User"
     email "test@example.com"
-    password "please123"
 
     trait :admin do
       role 'admin'
     end
 
+    trait :with_treasures do
+       after(:create) do |user|
+        FactoryGirl.create_list(:treasure, 10, user: user)
+      end
+    end
   end
 end
